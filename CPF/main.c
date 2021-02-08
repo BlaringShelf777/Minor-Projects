2a1cedcabd1e456f8cd802fa5ac885e11122d84a
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2021-02-07 21:25:32 -0300

    Creates CPF by an algorithm, Validation function fixed

--- CPF/main.c
+++ CPF/main.c
@@ -3,30 +3,21 @@
 #include <math.h>
 #include <time.h>
 #include <string.h>
-#include <inttypes.h>
 
 #define CPFSIZE 11
 
 char *createRandCpf(int size);
 void printfCpf(char *cpf);
 int validCpf(char *cpf);
+char *createCpf();
 
 int main(){
-    char *cpf = NULL;
-    int flag = 0;
-    unsigned long long int counter  = 0;
-
-    while (!flag){
-        cpf = createRandCpf(CPFSIZE);
-        if (validCpf(cpf)){
-            printfCpf(cpf);
-            flag = 1;
-        }
-        free (cpf);
-        counter++;
-    }
-
-    printf("\n Tries: %llu", counter);
+    char *cpf = createCpf();
+    
+    if (validCpf("04049223090"))
+        printfCpf(cpf);
+    else
+        printf("Invalid CPF.");
 
     return 0;
 }
@@ -65,9 +56,9 @@ void printfCpf(char *cpf){
 int validCpf(char *cpf){
     int dig1 = 0, dig2 = 0;
 
-    for (int i = 0, j = CPFSIZE; i < CPFSIZE; i++){
+    for (int i = 0, j = CPFSIZE; i < CPFSIZE - 1; i++){
         dig2 += (cpf[i] - '0') * j;
-        if (j - 1 > 2)
+        if (j - 1 > 1)
             dig1 += (cpf[i] - '0') * (j - 1);
         j--;
     }
@@ -77,10 +68,27 @@ int validCpf(char *cpf){
     return cpf[9] - '0' == dig1 && cpf[10] - '0' == dig2;
 }
 
-//0 * 11 + 4 * 10 + 0 * 9 + 4 * 8 + 9 * 7 + 2 * 6 + 2 * 5 + 3 * 4 + 0 * 3 + 9 * 2
+char *createCpf(){
+    char *cpf = (char *) malloc(sizeof(char) * (CPFSIZE + 1));
+    int dig1 = 0, dig2 = 0;
 
-//0 * 10 + 4 * 09 + 0 * 8 + 4 * 7 + 9 * 6 + 2 * 5 + 2 * 4 + 3 * 3 + 0 * 2
+    srand(time(NULL));
+    for (int i = 0, j = 10; i < 9; i++){
+        cpf[i] = '0' + rand() % 10;
+        dig1 += (cpf[i] - '0') * j;
+        j--;
+    }
+    dig1 = (dig1 * 10) % 11; dig1 = dig1 == 10 ? 0 : dig1;
+    cpf[9] = '0' + dig1;
+    for (int i = 0, j = 11; i < 10; i++){
+        dig2 += (cpf[i] - '0') * j;
+        j--;
+    }
+    dig2 = (dig2 * 10) % 11; dig2 = dig2 == 10 ? 0 : dig2;
+    cpf[10] = '0' + dig2;
 
+    return cpf;
+}
 
 
 
